0a3b14c8b3e28c831f054514de129864c7c2bfe1
elyudwo <dudqk9696@naver.com>
2025-12-07 00:23:33 +0900

    first commit

---
+++ src/main/kotlin/kr/example/eda_test/EdaTestApplication.kt
@@ -0,0 +1,11 @@
+package kr.example.eda_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EdaTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<EdaTestApplication>(*args)
+}
---
+++ src/test/kotlin/kr/example/eda_test/EdaTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.example.eda_test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EdaTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
